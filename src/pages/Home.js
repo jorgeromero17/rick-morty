f23943376dd3ff65bbbea1517cbfb50e1ad788e5
Jorge Romero <jorgeromero98@outlook.com>
2024-06-15 02:35:58 -0600

    Agregando stilos css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,22 +3,20 @@ import getData from "../utils/getData";
 const Home = async () => {
   const characters = await getData();
   const view = `
-    
+    <div class="Characters">
       ${characters.results
         .map(
           (character) => `
-          <div class="Characters">
-            <article class="Character-item">
-              <a href="#/${character.id}/">
-                <img src="${character.image}" alt="${character.name}">
-                <h2>${character.name}</h2>
-              </a>
-            </article>
-          </div>
-        `
+        <article class="Characters-item">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+      `
         )
-        .join()}
-      
+        .join("")}
+    </div>
   `;
   return view;
 };
